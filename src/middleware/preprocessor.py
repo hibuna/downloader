9bfd978ebe141fb7ed2ddcda7b7f0224684ab391
Angelo van Meurs <arcvanmeurs@hotmail.com>
2025-03-29 21:18:49 +0100

    Improve preprocessor structure

--- src/middleware/preprocessor.py
+++ src/middleware/preprocessor.py
@@ -1,13 +1,24 @@
+from abc import abstractmethod, ABC
+
 from src.api.request import RequestDownloadUrl
-from src.downloader.model import InputUrlModel
+from src.downloader.model import InputUrlModel, Dto
 
 
-class PreprocessorBase:
-    pass
+class PreprocessorBase(ABC):
+    output_type: Dto
+
+    @abstractmethod
+    def process(self, request) -> Dto: ...
+
+    @abstractmethod
+    def _create_dto(self, *args, **kwargs) -> Dto: ...
 
 
 class PreprocessorRequestDownloadUrl(PreprocessorBase):
     output_type = InputUrlModel
 
-    def process(self, request: RequestDownloadUrl):
-        return self.output_type(resource_handle=request.handle)
+    def process(self, request: RequestDownloadUrl) -> InputUrlModel:
+        return self._create_dto(handle=request.handle)
+
+    def _create_dto(self, handle: str) -> InputUrlModel:
+        return self.output_type(resource_handle=handle)
