b4c955ce045360d69e8bfa2c09a0292b520a5887
Angelo van Meurs <arcvanmeurs@hotmail.com>
2025-03-30 00:08:59 +0100

    Add rudimentary test

---
+++ src/api/test/test_api.py
@@ -0,0 +1,31 @@
+from litestar.status_codes import (
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
+from litestar.testing import TestClient
+
+from app import litestar_app
+
+
+def test_request_body_wrong_url():
+    with TestClient(app=litestar_app) as client:
+        response = client.post("/download/ur", json={"handle": "some handle"})
+        assert response.status_code == HTTP_404_NOT_FOUND
+
+
+def test_request_body_wrong_key():
+    with TestClient(app=litestar_app) as client:
+        response = client.post("/download/url", json={"andle": "some handle"})
+        assert response.status_code == HTTP_400_BAD_REQUEST
+
+
+def test_request_valid():
+    with TestClient(app=litestar_app) as client:
+        response = client.post("/download/url", json={"handle": "some handle"})
+        assert response.status_code == HTTP_201_CREATED
+
+
+test_request_body_wrong_url()
+test_request_body_wrong_key()
+test_request_valid()
